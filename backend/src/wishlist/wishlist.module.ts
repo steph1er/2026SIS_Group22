0fa4626ce342d5867dfbeb79283d002d523d5979
steph1er <stephanie.l.roberts@student.uts.edu.au>
2026-09-15 19:50:08 +1000

    set up wishlist api and completed get all items in wishlist

---
+++ backend/src/wishlist/wishlist.module.ts
@@ -0,0 +1,11 @@
+import { AuthModule } from "../auth/auth.module";
+import { Module } from '@nestjs/common';
+import { WishlistController } from "./wishlist.controller";
+import { WishlistService } from "./wishlist.service";
+
+@Module({
+    imports: [AuthModule],
+    controllers: [WishlistController],
+    providers: [WishlistService]
+})
+export class WishlistModule {}
