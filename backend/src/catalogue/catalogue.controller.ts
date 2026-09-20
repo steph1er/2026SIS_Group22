12947956cb79baf763999df615df1795c9b0b31a
steph1er <stephanie.l.roberts@student.uts.edu.au>
2026-09-20 18:12:30 +1000

    get catalogue item by id

--- backend/src/catalogue/catalogue.controller.ts
+++ backend/src/catalogue/catalogue.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { SupabaseAuthGuard } from "../auth/supabase-auth.guard";
 import { CatalogueService } from "./catalogue.service";
 
 @Controller('catalogue')
 @UseGuards(SupabaseAuthGuard)
 export class CatalogueController {
-
     constructor(private catalogueService: CatalogueService) {}
+
+    // get reccomended items for user
+
+    // get top 10 (check with group) items matching search criteria
+
+    @Get(':id')
+    getCatalogueItem(@Param('id') id: string) {
+        return this.catalogueService.getCatalogueItem(id);
+    }
 }
--- backend/src/catalogue/catalogue.service.ts
+++ backend/src/catalogue/catalogue.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
 import { SupabaseService } from "../supabase/supabase.service";
 
 @Injectable()
 export class CatalogueService {
     constructor(private readonly supabaseService: SupabaseService) {}
+
+    async getCatalogueItem(id: string){
+        const supabase = this.supabaseService.client;
+        
+        // get item by id 
+        const { data, error } = await supabase.from('catalogue_items')
+                                                .select('*')
+                                                .eq('id', id);
+
+        // if error return error message
+        if(error){
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // if no item throw not found exception
+        if(data.length === 0){
+            throw new NotFoundException('No item found with provided item id');
+        }
+
+        // otherwise item
+        return data;
+    }
 }
