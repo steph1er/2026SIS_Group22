5e371451ef531c940acb39c149063034e0b2e908
steph1er <stephanie.l.roberts@student.uts.edu.au>
2026-09-21 17:38:34 +1000

    get 10 reccomended catalogue items (not connected to machine learning)

--- backend/src/catalogue/catalogue.controller.ts
+++ backend/src/catalogue/catalogue.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { SupabaseAuthGuard } from "../auth/supabase-auth.guard";
 import { CatalogueService } from "./catalogue.service";
+import type { User } from "@supabase/supabase-js";
+import { AuthenticatedUser } from "../auth/authenticated-user.decorator";
 
 @Controller('catalogue')
 @UseGuards(SupabaseAuthGuard)
 export class CatalogueController {
     constructor(private catalogueService: CatalogueService) {}
 
-    // get reccomended items for user
+    @Get('reccomendations')
+    getCatalogueReccomendations(@AuthenticatedUser() user: User) {
+        return this.catalogueService.getCatalogueReccomendations(user.id);
+    }
 
     // get top 10 (check with group) items matching search criteria
 
--- backend/src/catalogue/catalogue.service.ts
+++ backend/src/catalogue/catalogue.service.ts
@@ -5,6 +5,28 @@ import { SupabaseService } from "../supabase/supabase.service";
 export class CatalogueService {
     constructor(private readonly supabaseService: SupabaseService) {}
 
+    async getCatalogueReccomendations(user_id: string) {
+        // TODO - implement properly with machine learning logic - for now just getting first 10 catalogue items
+        const supabase = this.supabaseService.client;
+
+        const { data, error } = await supabase.from('catalogue_items')
+                                                .select('*')
+                                                .limit(10);
+
+        // if error return error message
+        if(error){
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // if no item throw not found exception
+        if(data.length === 0){
+            throw new NotFoundException('No reccomended catalogue items found.');
+        }
+
+        // otherwise item
+        return data;
+    }
+
     async getCatalogueItem(id: string){
         const supabase = this.supabaseService.client;
         
